730bd5d3939f7ced4f0725e92ea95bc2443774e8
hideaki10 <senshikou1985@gmail.com>
2022-04-27 12:00:44 +0900

    add end to end test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,76 @@
 package main
 
 import (
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
 
-	rr := httptest.NewRecorder()
+	app := newApplication(t)
+	ts := newTestServer(t, app.routes())
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	sd, _, body := ts.get(t, "/ping")
 
-	ping(rr, r)
-
-	rs := rr.Result()
-
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	if sd != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, sd)
 	}
 
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
 	}
+	// app := &application{
+	// 	errorLog: log.New(io.Discard, "", 0),
+	// 	infoLog:  log.New(io.Discard, "", 0),
+	// }
+
+	// ts := httptest.NewTLSServer(app.routes())
+
+	// defer ts.Close()
+
+	// rs, err := ts.Client().Get(ts.URL + "/ping")
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// if rs.StatusCode != http.StatusOK {
+	// 	t.Errorf("want %d;got %d", http.StatusOK, rs.StatusCode)
+	// }
+
+	// defer rs.Body.Close()
+
+	// body, err := io.ReadAll(rs.Body)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// if string(body) != "OK" {
+	// 	t.Errorf("want body to equal %q", "OK")
+	// }
+
+	//rr := httptest.NewRecorder()
+
+	// r, err := http.NewRequest(http.MethodGet, "/", nil)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// ping(rr, r)
+
+	// rs := rr.Result()
+
+	// if rs.StatusCode != http.StatusOK {
+	// 	t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	// }
+
+	// defer rs.Body.Close()
+
+	// body, err := io.ReadAll(rs.Body)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// if string(body) != "OK" {
+	// 	t.Errorf("want body to equal %q", "OK")
+	// }
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -29,6 +29,9 @@ func (app *application) routes() http.Handler {
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc((app.loginUser)))
 	mux.Post("/user/logout", dynamicMiddleware.Append(app.requiredAuthentication).ThenFunc((app.logoutUser)))
 
+	// test
+	mux.Get("/ping", http.HandlerFunc(ping))
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
